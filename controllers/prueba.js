17ff7503b6d9d49487e8a6d36ac3f50d1ebfbb0c
matiguz <matiguz1990@gmail.com>
2018-10-23 00:43:18 -0300

    add axios to get request

--- controllers/prueba.js
+++ controllers/prueba.js
@@ -11,6 +11,27 @@ module.exports = {
             {latitude: , longitude: },
             {latitude: , longitude: }
         );*/
+
+        // Make a request for a user with a given ID
+        const axios = require('axios');
+
+        axios.get('http://tea.ddns.net:1026/v2/entities?type=Bus&limit=10')
+        .then(function (response) {
+            console.log(response.data.length);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+        axios.get('http://tea.ddns.net/api/trayectosporlinea')
+        .then(function (response) {
+            console.log(response.data.trayectos.length);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+
         let result = geoLib.distance({
             p1: { lat: -34.7844931, lon: -56.2239004 },
             p2: { lat: -34.799506, lon: -56.228390 }
