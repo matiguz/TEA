7242f8a065792a911b90d40ef46263080eaff278
Matiguz <matiguz1990@gmail.com>
2018-10-23 08:50:14 -0300

    set proxy and helper axios

--- controllers/prueba.js
+++ controllers/prueba.js
@@ -1,4 +1,5 @@
 let geoLib = require('geo-lib');
+const axios = require('../helpers/axios');
 
 module.exports = {
 
@@ -7,30 +8,22 @@ module.exports = {
     ***/
     
     prueba: (req, res) => {
-       /* let a = geolib.getDistance(
-            {latitude: , longitude: },
-            {latitude: , longitude: }
-        );*/
 
-        // Make a request for a user with a given ID
-        const axios = require('axios');
+     axios.inst.get('http://tea.ddns.net:1026/v2/entities?type=Bus&limit=10')
+      .then(function (response) {
+          console.log(response.data.length);
+      })
+      .catch(function (error) {
+          console.log(error);
+      });
 
-        axios.get('http://tea.ddns.net:1026/v2/entities?type=Bus&limit=10')
-        .then(function (response) {
-            console.log(response.data.length);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-
-        axios.get('http://tea.ddns.net/api/trayectosporlinea')
+      axios.inst.get('http://tea.ddns.net/api/trayectosporlinea')
         .then(function (response) {
             console.log(response.data.trayectos.length);
         })
         .catch(function (error) {
             console.log(error);
         });
-
 
         let result = geoLib.distance({
             p1: { lat: -34.7844931, lon: -56.2239004 },
