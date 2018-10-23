950c420ecf50cdaf128b242b07044faaddf742b1
matiguz <matiguz1990@gmail.com>
2018-10-22 22:17:56 -0300

    GeoLocation

--- controllers/prueba.js
+++ controllers/prueba.js
@@ -1,3 +1,4 @@
+let geoLib = require('geo-lib');
 
 module.exports = {
 
@@ -6,7 +7,15 @@ module.exports = {
     ***/
     
     prueba: (req, res) => {
-        
-        res.send("hola");
+       /* let a = geolib.getDistance(
+            {latitude: , longitude: },
+            {latitude: , longitude: }
+        );*/
+        let result = geoLib.distance({
+            p1: { lat: -34.7844931, lon: -56.2239004 },
+            p2: { lat: -34.799506, lon: -56.228390 }
+        });
+      //  res.send(distance);
+        res.send(result);
     }
 }
